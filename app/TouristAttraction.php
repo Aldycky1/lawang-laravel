2596532dc807d9054fe9ed99de16d1cf2415339f
Aldycky1 <bagusaldycky@gmail.com>
2021-09-02 17:56:08 +0700

    updated

--- app/TouristAttraction.php
+++ app/TouristAttraction.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TouristAttraction extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name', 'description', 'address', 'slug', 'facilities_id', 'tourist_packages_id', 'hotels_id', 'capacities_id', 'regulations_id'
+    ];
+
+    protected $hidden = [];
 }
